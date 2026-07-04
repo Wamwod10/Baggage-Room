44b58da0d48b296d51a2164827921beb5fd6511e
Wamwod10 <shamshodochilov160@gmail.com>
2026-07-04 16:21:10 +0500

    final back push

--- src/services/shiftService.js
+++ src/services/shiftService.js
@@ -60,7 +60,7 @@ const shiftService = {
 
   async sendCurrentSalesTelegram(branchName = null) {
     const branchId = await branchService.getBranchIdByName(branchName);
-    const response = await apiClient.post("/shifts/current/sales-telegram", null, { params: { branchId } });
+    const response = await apiClient.post("/shifts/current/sales-telegram", {}, { params: { branchId } });
     return getData(response);
   },
 
